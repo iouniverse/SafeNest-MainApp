3829f606929cf270783999f7ae8e32ccec151127
XusanDev07 <133357417+XusanDev07@users.noreply.github.com>
2025-02-06 02:16:42 +0500

    Minor change made.

--- config/celery.py
+++ config/celery.py
@@ -11,13 +11,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 app = Celery('config')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-@app.on_after_configure.connect
-def setup_periodic_tasks(sender, **kwargs):
-    from apps.core.models import Camera
-    cameras = Camera.objects.filter(status=True)
-    for camera in cameras:
-        StreamManager.start_stream(camera)
-
 app.autodiscover_tasks()
 
 @app.task(bind=True)
