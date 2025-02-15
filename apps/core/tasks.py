c23b83b669e3fd9d30e9292417a7f8fddfc2b0ec
XusanDev07 <133357417+XusanDev07@users.noreply.github.com>
2025-02-15 15:31:36 +0500

    Camera problem solved, recording automatically

--- apps/core/tasks.py
+++ apps/core/tasks.py
@@ -8,10 +8,6 @@ def create_camera_and_start_stream(camera_id):
     print(f"Received task to start stream for Camera ID: {camera_id}")
     try:
         camera = Camera.objects.get(id=camera_id)
-        print(f"Camera found: {camera}")
         StreamManager.start_stream(camera)
-        print("Stream started successfully!")
-    except Camera.DoesNotExist:
-        print(f"Camera with ID {camera_id} does not exist")
     except Exception as e:
         print(f"Error while starting stream: {e}")
